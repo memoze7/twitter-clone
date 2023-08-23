236d1f5ec9aeb90d29847ee72c7261b9866bdaf6
Guillermo Perez <guillermojperezp@gmail.com>
2023-08-23 11:14:51 -0400

    create project and connect with supabase

---
+++ src/app/layout.tsx
@@ -0,0 +1,19 @@
+import './globals.css'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Clon de twitter',
+  description: 'Generado moviendo las manitas'
+}
+
+export default function RootLayout ({
+  children
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
